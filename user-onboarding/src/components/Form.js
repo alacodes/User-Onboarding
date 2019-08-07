f020d4405eeb219d3a35a1dd670932fed910cfce
Tracie T <traciecodes@gmail.com>
2019-08-07 17:20:28 -0500

    3rd(?): MVP met, step 4 was a pain.

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,32 +1,86 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { withFormik, Form, Field } from 'formik'
+import axios from 'axios'
 import * as Yup from 'yup'
+import './form.css'
 
-const UserForm = (values) => (
-    <Form>
-        <Field type="name" name="name" placeholder="Name" />
-        <Field type="email" name="email" placeholder="Email" />
-        <Field type="password" name="password" placeholder="Password" />
-        <label>
-            We both know you didn't read the terms:
-            <Field type="checkbox" name="terms" checked={values.newsletter}/>
-        </label>
-        <button>Submit!</button>
-    </Form>
-)
+const UserForm = ({values, errors, touched, isSubmitting, status}) => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        if (status) {
+            setUsers([...users, status]);
+        }
+    }, [status]);
+    return (
+    <>
+        <Form className="form">
+            <Field className="field" type="name" name="name" placeholder="Name" />
+            <div className="field">
+                <Field type="email" name="email" placeholder="Email" />
+                { touched.email && errors.email && <p>{errors.email}</p> }
+            </div>
+            <div className="field">
+                <Field type="password" name="password" placeholder="Password" />
+                { touched.password && errors.password && <p>{errors.password}</p> }
+            </div>
+            <Field component="select" name="dropdown" className="dropdown">
+                <option value="favoriteThings">Please select your favorite:</option>
+                <option value="freeStuff">Free Stuff</option>
+                <option value="iceCream">Ice cream</option>
+                <option value="all">Porque no los dos</option>
+            </Field>
+            <label>
+                We both know you didn't read the terms:
+                <Field type="checkbox" name="terms" checked={values.terms}/>
+            </label>
+            <button type="submit" disabled={isSubmitting}>Submit!</button>
+            <div className="currentUsers">
+                <h1>Current Friends</h1>
+                {values.user && values.users.map(user => console.log('These users: ', user))}
+                {users ? users.map(user => (
+                    <p key={user.id} className="users">{user.name}</p>
+                ))
+                : null}
+            </div>
+        </Form>
+        </>
+    )
+}
 
 const FormikForm = withFormik ({
-    mapPropsToValues({name, email, password, terms}) {
+    mapPropsToValues({name, email, password, dropdown, terms, users}) {
         return{
             name: name || '',
             email: email || '',
             password: password || '',
-            terms: terms || false
+            dropdown: dropdown,
+            terms: terms || false,
+            users: ['Example']
         }
     },
-    handleSubmit(values){
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string().email('Email not valid').required('Email required'),
+        password: Yup.string().min(8, 'Password must be at least 8 characters. You know. For safety.').required(),
+        terms: Yup.bool().oneOf([true], 'Please check it anyway')
+    }),
+    handleSubmit(values, {resetForm, setErrors, setSubmitting, setStatus}){
+        axios
+            .post('https://reqres.in/api/users/', values)
+            .then(res => {
+                setStatus(res.data);
+            })
+        setTimeout(() => {
+            if(values.email === 'tea@tea.io'){
+                setErrors({ email: 'That email is already registered'})
+            } else {
+                resetForm()
+            }
+            setSubmitting(false)
+        }, 2000)
         console.log(values)
-    }
+        }
+      
 })(UserForm)
 
 export default FormikForm
